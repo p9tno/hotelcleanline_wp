b5b529b552859c35dd1f908ac79d7efe811a32e7
p9tno <plahotin_s@mail.ru>
2026-04-16 14:27:06 +0300

    add demo content firstscreen__content

---
+++ template-testpage.php
@@ -0,0 +1,188 @@
+<?php
+/**
+ * Template name: Демо контент
+ */
+?>
+
+<?php get_header(); ?>
+
+
+    <!-- begin firstscreen-->
+    <section class="firstscreen section" id="firstscreen">
+        <div class="firstscreen__swiper">
+            <div class="section__title" style="margin: 1em">Вариации с разной тенью</div>
+            
+            <div class="swiper firstscreen_swiper_js">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="firstscreen__slide demo_grid">
+                            <div class="firstscreen__img img">
+                                <img src="<?php echo get_template_directory_uri() . '/assets/img/demo.png' ?>" alt="">
+                            </div>
+
+                            <div class="firstscreen__content v_1">
+                                <h2 class="firstscreen__title">Изысканая забота</h2>
+                                <div class="firstscreen__desc">
+                                    <p>Ежедневный уход</p>
+                                </div>
+                                <div class="firstscreen__btn">
+                                    <a class="btn" href="#">Подробнее</a>
+                                </div>
+                            </div>
+                            <div class="firstscreen__content v_2">
+                                <h2 class="firstscreen__title">Изысканая забота</h2>
+                                <div class="firstscreen__desc">
+                                    <p>Ежедневный уход</p>
+                                </div>
+                                <div class="firstscreen__btn">
+                                    <a class="btn" href="#">Подробнее</a>
+                                </div>
+                            </div>
+                            <div class="firstscreen__content v_3">
+                                <h2 class="firstscreen__title">Изысканая забота</h2>
+                                <div class="firstscreen__desc">
+                                    <p>Ежедневный уход</p>
+                                </div>
+                                <div class="firstscreen__btn">
+                                    <a class="btn" href="#">Подробнее</a>
+                                </div>
+                            </div>
+                            <div class="firstscreen__content v_4">
+                                <h2 class="firstscreen__title">Изысканая забота</h2>
+                                <div class="firstscreen__desc">
+                                    <p>Ежедневный уход</p>
+                                </div>
+                                <div class="firstscreen__btn">
+                                    <a class="btn" href="#">Подробнее</a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end firstscreen-->
+    <!-- begin firstscreen-->
+    <section class="firstscreen section" id="firstscreen">
+        <div class="firstscreen__swiper">
+            <div class="section__title" style="margin: 1em">Вариации с разной тенью + бордер</div>
+            
+            <div class="swiper firstscreen_swiper_js">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="firstscreen__slide demo_grid">
+                            <div class="firstscreen__img img">
+                                <img src="<?php echo get_template_directory_uri() . '/assets/img/demo.png' ?>" alt="">
+                            </div>
+                            <div class="firstscreen__content v_5">
+                                <h2 class="firstscreen__title">Изысканая забота</h2>
+                                <div class="firstscreen__desc">
+                                    <p>Ежедневный уход</p>
+                                </div>
+                                <div class="firstscreen__btn">
+                                    <a class="btn" href="#">Подробнее</a>
+                                </div>
+                            </div>
+                            <div class="firstscreen__content v_6">
+                                <h2 class="firstscreen__title">Изысканая забота</h2>
+                                <div class="firstscreen__desc">
+                                    <p>Ежедневный уход</p>
+                                </div>
+                                <div class="firstscreen__btn">
+                                    <a class="btn" href="#">Подробнее</a>
+                                </div>
+                            </div>
+                            <div class="firstscreen__content v_7">
+                                <h2 class="firstscreen__title">Изысканая забота</h2>
+                                <div class="firstscreen__desc">
+                                    <p>Ежедневный уход</p>
+                                </div>
+                                <div class="firstscreen__btn">
+                                    <a class="btn" href="#">Подробнее</a>
+                                </div>
+                            </div>
+                            <div class="firstscreen__content v_8">
+                                <h2 class="firstscreen__title">Изысканая забота</h2>
+                                <div class="firstscreen__desc">
+                                    <p>Ежедневный уход</p>
+                                </div>
+                                <div class="firstscreen__btn">
+                                    <a class="btn" href="#">Подробнее</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end firstscreen-->
+
+    <!-- begin firstscreen-->
+    <section class="firstscreen section" id="firstscreen">
+        <div class="firstscreen__swiper">
+            <div class="section__title" style="margin: 1em">Более сложный варианты</div>
+            
+            <div class="swiper firstscreen_swiper_js">
+                <div class="swiper-wrapper">
+                    <div class="swiper-slide">
+                        <div class="firstscreen__slide demo_grid">
+                            <div class="firstscreen__img img">
+                                <img src="<?php echo get_template_directory_uri() . '/assets/img/demo.png' ?>" alt="">
+                            </div>
+                            <div class="firstscreen__content v_9">
+                                <h2 class="firstscreen__title">Изысканая забота</h2>
+                                <div class="firstscreen__desc">
+                                    <p>Ежедневный уход</p>
+                                </div>
+                                <div class="firstscreen__btn">
+                                    <a class="btn" href="#">Подробнее</a>
+                                </div>
+                                
+                            </div>
+                            <div class="firstscreen__content v_10">
+                                <h2 class="firstscreen__title">Изысканая забота</h2>
+                                <div class="firstscreen__desc">
+                                    <p>Ежедневный уход</p>
+                                </div>
+                                <div class="firstscreen__btn">
+                                    <a class="btn" href="#">Подробнее</a>
+                                </div>
+                                
+                            </div>
+                            <div class="firstscreen__content v_11">
+                                <h2 class="firstscreen__title">Изысканая забота</h2>
+                                <div class="firstscreen__desc">
+                                    <p>Ежедневный уход</p>
+                                </div>
+                                <div class="firstscreen__btn">
+                                    <a class="btn" href="#">Подробнее</a>
+                                </div>
+                                
+                            </div>
+                            <div class="firstscreen__content v_12">
+                                <h2 class="firstscreen__title">Изысканая забота</h2>
+                                <div class="firstscreen__desc">
+                                    <p>Ежедневный уход</p>
+                                </div>
+                                <div class="firstscreen__btn">
+                                    <a class="btn" href="#">Подробнее</a>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end firstscreen-->
+
+
+
+
+
+
+<?php get_footer(); ?>
