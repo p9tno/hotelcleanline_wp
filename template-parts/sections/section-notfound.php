e3cd197e66c3c88717cd7ccef501887a844e1d8f
p9tno <plahotin_s@mail.ru>
2026-04-22 14:11:52 +0300

    add 404 page

---
+++ template-parts/sections/section-notfound.php
@@ -0,0 +1,24 @@
+<!-- begin notfound -->
+<section class="notfound section" id="notfound">
+    <div class="container_center">
+        <div class="notfound__wrap">
+            <div class="notfound__404">
+                <span>404</span>    
+           </div>
+           <div class="notfound__content">
+               <h1 class="section__title">Страница не найдена</h1>
+               <div class="section__content">
+                   <p>
+                       Возможно, товар был удалён из каталога, страница перемещена<br>
+                       или вы перешли по неверной ссылке.
+                   </p>
+               </div>
+               <div class="section__btns">
+                   <a class="btn" href="<?php echo esc_url(home_url('/')); ?>">На главную</a>
+                   <!-- <a class="btn btn_border" href="<?php // echo esc_url(home_url('/catalog/')); ?>">Перейти в каталог</a> -->
+               </div>
+           </div>
+        </div>
+    </div>
+</section>
+<!-- end notfound -->
